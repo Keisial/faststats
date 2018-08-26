42318452f3246655d77ad7261050da2365f276a5
Ángel González <keisial@gmail.com>
2018-08-27 00:05:28 +0200

    Convert tabs to spaces

--- faststats-cli.c
+++ faststats-cli.c
@@ -23,61 +23,61 @@
 #include "faststats.h"
 
 int main(int argc, char **argv) {
-	int index;
-	int is_signed = 1;
+    int index;
+    int is_signed = 1;
 
-	if (argc < 3) {
-		fprintf(stderr, "Usage: %s <filename> <index> [[+|-|=]<value>]\n", argv[0]);
-		return 1;
-	}
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <filename> <index> [[+|-|=]<value>]\n", argv[0]);
+        return 1;
+    }
 
-	const int max_index = faststats_max_index();
-	char *endptr;
-	errno = 0;
-	index = strtol(argv[2], &endptr, 10);
-	if (errno || *endptr || index < 0 || index > max_index) {
-		fprintf(stderr, "The index must be a number in [0, %d]\n", max_index);
-		return 2;
-	}
+    const int max_index = faststats_max_index();
+    char *endptr;
+    errno = 0;
+    index = strtol(argv[2], &endptr, 10);
+    if (errno || *endptr || index < 0 || index > max_index) {
+        fprintf(stderr, "The index must be a number in [0, %d]\n", max_index);
+        return 2;
+    }
 
-	faststats_init(argv[1]);
+    faststats_init(argv[1]);
 
-	if (argc < 4) {
-		printf(is_signed ? "Value at %d is %lld\n" : "Value at %d is %llu\n", index, faststats_read(index));
-	} else {
-		const char* action, *value_description = "it is now";
-		FASTSTATS_TYPE (*faststats_function)(int index, FASTSTATS_TYPE value);
+    if (argc < 4) {
+        printf(is_signed ? "Value at %d is %lld\n" : "Value at %d is %llu\n", index, faststats_read(index));
+    } else {
+        const char* action, *value_description = "it is now";
+        FASTSTATS_TYPE (*faststats_function)(int index, FASTSTATS_TYPE value);
 
-		if (argv[3][0] == '+') {
-			action = "Added";
-			faststats_function = faststats_add;
-			argv[3]++;
-		} else if (argv[3][0] == '-') {
-			action = "Substracted";
-			faststats_function = faststats_sub;
-			argv[3]++;
-		} else if (argv[3][0] == '=') {
-			action = "Stored";
-			faststats_function = faststats_store;
-			argv[3]++;
-		} else {
-			action = "Saved";
-			value_description = "it used to be";
-			faststats_function = faststats_exchange;
-		}
+        if (argv[3][0] == '+') {
+            action = "Added";
+            faststats_function = faststats_add;
+            argv[3]++;
+        } else if (argv[3][0] == '-') {
+            action = "Substracted";
+            faststats_function = faststats_sub;
+            argv[3]++;
+        } else if (argv[3][0] == '=') {
+            action = "Stored";
+            faststats_function = faststats_store;
+            argv[3]++;
+        } else {
+            action = "Saved";
+            value_description = "it used to be";
+            faststats_function = faststats_exchange;
+        }
 
-		errno = 0;
-		FASTSTATS_TYPE value = strtoll(argv[3], &endptr, 10);
-		if (errno || *endptr) {
-			fprintf(stderr, "Bad value %s\n", argv[3]);
-			return 3;
-		}
+        errno = 0;
+        FASTSTATS_TYPE value = strtoll(argv[3], &endptr, 10);
+        if (errno || *endptr) {
+            fprintf(stderr, "Bad value %s\n", argv[3]);
+            return 3;
+        }
 
-		FASTSTATS_TYPE new_value = faststats_function(index, value);
-		printf(is_signed ? "%s %lld at %d, %s %lld\n" :
-			"%s %llu at %d, %s %llu\n", action, value, index, value_description, new_value);
-	}
+        FASTSTATS_TYPE new_value = faststats_function(index, value);
+        printf(is_signed ? "%s %lld at %d, %s %lld\n" :
+            "%s %llu at %d, %s %llu\n", action, value, index, value_description, new_value);
+    }
 
-	faststats_finit();
-	return 0;
+    faststats_finit();
+    return 0;
 }
--- faststats.c
+++ faststats.c
@@ -27,48 +27,48 @@
 static FASTSTATS_TYPE *memory_block = NULL;
 
 int faststats_init(const char* filename) {
-	void *memory;
-	key_t key = ftok(filename, 'S');
-	if (key == -1) {
-		return -1;
-	}
+    void *memory;
+    key_t key = ftok(filename, 'S');
+    if (key == -1) {
+        return -1;
+    }
 
-	int shmid = shmget(key, PAGE_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	
-	memory = shmat(shmid, NULL, 0);
-	if (memory == (void*)-1) {
-		return -1;
-	}
-	memory_block = memory;
-	
-	return 0;
+    int shmid = shmget(key, PAGE_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    
+    memory = shmat(shmid, NULL, 0);
+    if (memory == (void*)-1) {
+        return -1;
+    }
+    memory_block = memory;
+    
+    return 0;
 }
 
 int faststats_finit() {
-	return shmdt(memory_block);
+    return shmdt(memory_block);
 }
 
 int __attribute__((const)) faststats_max_index(void) {
-	return (PAGE_SIZE -1) / sizeof(FASTSTATS_TYPE);
+    return (PAGE_SIZE -1) / sizeof(FASTSTATS_TYPE);
 }
 
 FASTSTATS_TYPE faststats_read(int index) {
-	return __atomic_load_n(&memory_block[index], __ATOMIC_SEQ_CST);
+    return __atomic_load_n(&memory_block[index], __ATOMIC_SEQ_CST);
 }
 
 FASTSTATS_TYPE faststats_store(int index, FASTSTATS_TYPE value) {
-	__atomic_store_n(&memory_block[index], value, __ATOMIC_SEQ_CST);
-	return value;
+    __atomic_store_n(&memory_block[index], value, __ATOMIC_SEQ_CST);
+    return value;
 }
 
 FASTSTATS_TYPE faststats_exchange(int index, FASTSTATS_TYPE value) {
-	return __atomic_exchange_n(&memory_block[index], value, __ATOMIC_SEQ_CST);
+    return __atomic_exchange_n(&memory_block[index], value, __ATOMIC_SEQ_CST);
 }
 
 FASTSTATS_TYPE faststats_add(int index, FASTSTATS_TYPE value) {
-	return __atomic_add_fetch(&memory_block[index], value, __ATOMIC_SEQ_CST);
+    return __atomic_add_fetch(&memory_block[index], value, __ATOMIC_SEQ_CST);
 }
 
 FASTSTATS_TYPE faststats_sub(int index, STATS_TYPE value) {
-	return __atomic_sub_fetch(&memory_block[index], value, __ATOMIC_SEQ_CST);
+    return __atomic_sub_fetch(&memory_block[index], value, __ATOMIC_SEQ_CST);
 }
