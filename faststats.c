3d808b3716c4357423c5aedbc976b97061c24bd4
Ángel González <keisial@gmail.com>
2018-08-27 00:03:44 +0200

    Add license notices

--- faststats.c
+++ faststats.c
@@ -1,3 +1,21 @@
+/**
+ * This file is part of faststats.
+ * Copyright (C) 25-26/8/2018 Ángel González
+ *
+ * faststats is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * faststats is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ * along with faststats.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <sys/types.h>
 #include <sys/user.h>
 #include <sys/shm.h>
